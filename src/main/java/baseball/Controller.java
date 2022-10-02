fa25e04503f5cd71921c96986e965285b017f54d
jalhagosipo <gpfla757@naver.com>
2022-10-02 23:05:25 +0900

    refactor: static END_GAME 변수 사용

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -3,6 +3,7 @@ package baseball;
 public class Controller {
 
     private static String REGEX_PATTERN = "[1-9]{3}";
+    private static String END_GAME = "2";
     private String input;
 
     void validateNumber(String number) {
@@ -19,7 +20,7 @@ public class Controller {
     }
 
     boolean finishGame(String endInput) {
-        if (endInput.equals("2")) {
+        if (endInput.equals(END_GAME)) {
             return true;
         }
         return false;
