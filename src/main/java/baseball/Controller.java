9b811296bcaf8c5cf92bffb9770db5b8993edc38
jalhagosipo <gpfla757@naver.com>
2022-10-02 22:03:50 +0900

    feat: 게임을 다시 시작하거나 종료시킬 수 있도록 만들기
    - 답을 비교하는 껍데기 함수를 호출하고 게임 종료하는 함수를 호출
    - 게임이 끝나고 다시 할지 말지 정할 수 있는 로직 구현

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -3,11 +3,25 @@ package baseball;
 public class Controller {
 
     private static String REGEX_PATTERN = "[1-9]{3}";
+    private String input;
 
-    String validateNumber(String number) {
-        if (number.matches(REGEX_PATTERN)) {
-            return number;
+    void validateNumber(String number) {
+        if (!number.matches(REGEX_PATTERN)) {
+            throw new IllegalArgumentException();
         }
-        throw new IllegalArgumentException();
+        this.input = number;
+    }
+
+    boolean checkAnswer() {
+        // TODO 힌트 출력 -> 볼 or 스트라이크 or 낫싱
+        // 답맞춤
+        return true;
+    }
+
+    boolean finishGame(String endInput) {
+        if (endInput.equals("2")) {
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/baseball/UI.java
+++ src/main/java/baseball/UI.java
@@ -4,15 +4,29 @@ import java.util.Scanner;
 
 public class UI {
     Scanner scanner = new Scanner(System.in);
+    Controller controller = new Controller();
+    boolean isAnswer;
+    boolean finishGame = false;
 
     void start() {
-        Controller controller = new Controller();
 
-        while (true) {
+        while (!finishGame) {
             System.out.print("숫자를 입력해주세요 : ");
             String input = scanner.next();
+
             controller.validateNumber(input);
-            System.out.println("힌트");
+
+            isAnswer = controller.checkAnswer();
+
+            finishGame(isAnswer);
+        }
+    }
+
+    private void finishGame(boolean isAnswer) {
+        if (isAnswer) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String input = scanner.next();
+            finishGame = controller.finishGame(input);
         }
     }
 }
