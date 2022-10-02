bdd1f0caabd79ac9d0c88c2659c17c1125cdd52d
jalhagosipo <gpfla757@naver.com>
2022-10-02 22:59:34 +0900

    fix: 예외 잡지 않도록 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,10 +3,6 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         UI ui = new UI();
-        try {
-            ui.start();
-        } catch (Exception e) {
-            return;
-        }
+        ui.start();
     }
 }
